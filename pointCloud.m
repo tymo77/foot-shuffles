ceea447aee71795914176cbeef5854cbd432e9c8
Tyler Morrison <tylermorrison7777@gmail.com>
2017-12-08 23:03:58 -0500

    revisions to testing script
    small revisions only - important to use simpler triangularization method

--- pointCloud.m
+++ pointCloud.m
@@ -9,7 +9,7 @@ bodyRot=[0,0,0];
 bodyW=1;
 bodyL=1;
 %% Workspace point cloud
-N=20;
+N=12;
 deg70=deg2rad(70);
 th1=linspace(-deg70,deg70,N);
 th2=th1;
@@ -19,15 +19,15 @@ scatter3(x,y,z)
 axis equal
 
 workspacePts=[x y z];
-tri = delaunayTriangulation(x,y,z);
-trisurf(tri.ConnectivityList,x,y,z);
+tri = delaunay(x,y,z);
+trisurf(tri,x,y,z);
 workspaceFaces=meshFaces(tri.ConnectivityList);
 axis equal
 
 %% surface
-Nsurf=30;xbnds=[-3,3];ybnds=[-3,3];
-surfPts=generateSurfacePoints(Nsurf,xbnds,ybdns,'wave');
+Nsurf=20;xbnds=[-3,3];ybnds=[-3,3];
 
+surfPts=generateSurfacePoints(Nsurf,xbnds,ybnds,'wave');
 sx=surfPts(:,1);sy=surfPts(:,2);sz=surfPts(:,3);
 
 scatter3(sx,sy,sz)
@@ -35,43 +35,38 @@ triSurf=delaunayTriangulation(sx,sy);
 trisurf(triSurf.ConnectivityList,sx,sy,sz)
 
 %% intersection
-surfPts=[sx sy sz];
 
-intPts=mesh2mesh(workspaceFaces,workspacePts,triSurf.ConnectivityList,surfPts);
+Nsurf=15;
+%box in shadow of workspace
+xbnds=[min(workspacePts(:,1)),max(workspacePts(:,1))];
+ybnds=[min(workspacePts(:,2)),max(workspacePts(:,2))];
 
-scatter3(pts(:,1),pts(:,2),pts(:,3))
+surfPts=generateSurfacePoints(Nsurf,xbnds,ybnds,'wave');
+sx=surfPts(:,1);sy=surfPts(:,2);sz=surfPts(:,3);
+triSurf=delaunay(sx,sy);
+
+intPts=mesh2mesh(workspaceFaces,workspacePts,triSurf,surfPts);
+
+scatter3(intPts(:,1),intPts(:,2),intPts(:,3))
 hold on
 scatter3(x,y,z,'r')
 axis equal
 
 
-interSectTri=delaunayTriangulation(pts(:,1),pts(:,2));
+interSectTri=delaunay(intPts(:,1),intPts(:,2));
 figure
 hold on
-triplot(interSectTri)
-scatter3(pts(:,1),pts(:,2),pts(:,3))
-axis equal
-
-%% testing Filter
-filterPts=filterPtsByInvKin(workspacePts-bodyPos,-deg70,deg70,bodyW,bodyL,distalLen,proximalLen,1);
-
-figure
-scatter3(workspacePts(:,1),workspacePts(:,2),workspacePts(:,3),'k');
-hold on
-scatter3(filterPts(:,1),filterPts(:,2),filterPts(:,3),'rx');
-axis equal
-
-figure
-scatter3(filterPts(:,1),filterPts(:,2),filterPts(:,3),'rx');
+scatter3(intPts(:,1),intPts(:,2),intPts(:,3))
 axis equal
 %% filter intersection
-filterPts=filterPtsByInvKin(pts-bodyPos,-deg70,deg70,bodyW,bodyL,distalLen,proximalLen,1);
-
+filterPts=filterPtsByInvKin(intPts-bodyPos,-deg70,deg70,bodyW,bodyL,distalLen,proximalLen,1);
 filterTri=delaunay(filterPts(:,1),filterPts(:,2));
+
+figure
 trisurf(filterTri,filterPts(:,1),filterPts(:,2),filterPts(:,3))
 axis equal
 
-filterTri2=filterTriByCentroid(filterTri,pts-bodyPos,-deg70,deg70,bodyW,bodyL,distalLen,proximalLen,1);
-
+filterTri2=filterTriByCentroid(filterTri,filterPts-bodyPos,-deg70,deg70,bodyW,bodyL,distalLen,proximalLen,1);
+figure
 trisurf(filterTri2,filterPts(:,1),filterPts(:,2),filterPts(:,3))
 axis equal
