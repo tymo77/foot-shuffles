df34a6e695dcc92513c394009e196b8edd229561
Tyler Morrison <morrison.730@mecheng.osu.edu>
2017-12-14 10:02:38 -0500

    revised kinematics
    footPosForwardKinematics needed to be converted to the robot struct format
    
    kneeUpAngles is added to pick the knee up configuration

--- kinematics/footPosForwardKinematics.m
+++ kinematics/footPosForwardKinematics.m
@@ -1,29 +1,28 @@
-function [positions]=footPosForwardKinematics(thX,thY,thZ,bodyX,bodyY,bodyZ,jointAngles,wBodyInput,lenBodyInput,distalLen,proximalLen,legNo)
+function p=footPosForwardKinematics(jointAngles,legNo,robot)
 
-if (~exist('legNo', 'var'))
-    N=4;
-    Nstart=1;
-else
-    N=legNo;
-    Nstart=legNo;
-    temp=zeros(4,3);
-    temp(legNo,:)=jointAngles;
-    jointAngles=temp;
-end
-    
-positions=zeros(N,3);
-for i=Nstart:N
-    th11 = jointAngles(i, 1);
-    th12 = jointAngles(i, 2);
-    th13 = jointAngles(i, 3);
-    WBody = wBodyInput*sqrt(2)*sin(1/2*pi*(i - 1) + pi/4) ;
-    LBody = lenBodyInput*sqrt(2)*cos(1/2*pi*(i - 1) + pi/4) ;
-    
-    positions(i,:)=...
-        [...
-        bodyX - (proximalLen*sin(th12) + distalLen*sin(th12 + th13))*sin(thY) + (cos(thY)*(LBody*cos(thZ) + 2*(proximalLen*cos(th12) + distalLen*cos(th12 + th13))*cos(th11 + thZ) - WBody*sin(thZ)))/2,...
-        bodyY + cos(thY)*(proximalLen*sin(th12) + distalLen*sin(th12 + th13))*sin(thX) + (sin(thX)*sin(thY)*(LBody*cos(thZ) + 2*(proximalLen*cos(th12) + distalLen*cos(th12 + th13))*cos(th11 + thZ) - WBody*sin(thZ)) + cos(thX)*(WBody*cos(thZ) + LBody*sin(thZ) + 2*(proximalLen*cos(th12) + distalLen*cos(th12 + th13))*sin(th11 + thZ)))/2,...
-        bodyZ - (cos(thX)*(2*cos(thY)*(proximalLen*sin(th12) + distalLen*sin(th12 + th13)) + sin(thY)*(LBody*cos(thZ) + 2*(proximalLen*cos(th12) + distalLen*cos(th12 + th13))*cos(th11 + thZ) - WBody*sin(thZ))))/2 + (sin(thX)*(WBody*cos(thZ) + LBody*sin(thZ) + 2*(proximalLen*cos(th12) + distalLen*cos(th12 + th13))*sin(th11 + thZ)))/2,...
-        ];
-end
+thX=robot.bodyRot(1);
+thY=robot.bodyRot(2);
+thZ=robot.bodyRot(3);
+bodyX=robot.bodyPos(1);
+bodyY=robot.bodyPos(2);
+bodyZ=robot.bodyPos(3);
+wBodyInput=robot.bodyW;
+lenBodyInput=robot.bodyL;
+distalLen=robot.distLen;
+proximalLen=robot.proxLen;
+i=legNo;
+
+th11 = jointAngles(1);
+th12 = jointAngles(2);
+th13 = jointAngles(3);
+WBody = wBodyInput*sqrt(2)*sin(1/2*pi*(i - 1) + pi/4) ;
+LBody = lenBodyInput*sqrt(2)*cos(1/2*pi*(i - 1) + pi/4) ;
+
+p=...
+    [...
+    bodyX - (proximalLen*sin(th12) + distalLen*sin(th12 + th13))*sin(thY) + (cos(thY)*(LBody*cos(thZ) + 2*(proximalLen*cos(th12) + distalLen*cos(th12 + th13))*cos(th11 + thZ) - WBody*sin(thZ)))/2,...
+    bodyY + cos(thY)*(proximalLen*sin(th12) + distalLen*sin(th12 + th13))*sin(thX) + (sin(thX)*sin(thY)*(LBody*cos(thZ) + 2*(proximalLen*cos(th12) + distalLen*cos(th12 + th13))*cos(th11 + thZ) - WBody*sin(thZ)) + cos(thX)*(WBody*cos(thZ) + LBody*sin(thZ) + 2*(proximalLen*cos(th12) + distalLen*cos(th12 + th13))*sin(th11 + thZ)))/2,...
+    bodyZ - (cos(thX)*(2*cos(thY)*(proximalLen*sin(th12) + distalLen*sin(th12 + th13)) + sin(thY)*(LBody*cos(thZ) + 2*(proximalLen*cos(th12) + distalLen*cos(th12 + th13))*cos(th11 + thZ) - WBody*sin(thZ))))/2 + (sin(thX)*(WBody*cos(thZ) + LBody*sin(thZ) + 2*(proximalLen*cos(th12) + distalLen*cos(th12 + th13))*sin(th11 + thZ)))/2,...
+    ];
+
 end
