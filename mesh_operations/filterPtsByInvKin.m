ce9b31b00c96880b05e13d58191460299f11fa15
Tyler Morrison <morrison.730@mecheng.osu.edu>
2017-12-14 10:00:55 -0500

    bug fix - filtering not working for legs 2 and 3
    Angle 1 needs to be wrapped differently so that it handles legs 2 and 3 correctly.

--- mesh_operations/filterPtsByInvKin.m
+++ mesh_operations/filterPtsByInvKin.m
@@ -1,11 +1,6 @@
 function [feasiblePts,mask]=filterPtsByInvKin(pts,minAngles,maxAngles,...
     LegNo,robot)
 
-WBody=robot.bodyW;
-LBody=robot.bodyL;
-LProximal=robot.proxLen;
-LDistal=robot.distLen;
-
 if length(minAngles)==1
     minAngles=[minAngles minAngles minAngles];
 end
@@ -17,34 +12,34 @@ end
 nPts=size(pts,1);
 mask=zeros(nPts,1);
 for i=1:nPts
-    mask(i)=testPoint(pts(i,:));
+    mask(i)=testPoint(pts(i,:),minAngles,maxAngles,LegNo,robot);
 end
 
 feasiblePts=pts(logical(mask),:);
+end
 
+function feasible=testPoint(p,minAngles,maxAngles,LegNo,robot)
+px=p(1);py=p(2);pz=p(3);
+[angles,flags]=computeInverseKinematics(px, py, pz, LegNo, robot);
 
-    function feasible=testPoint(p)
-        px=p(1);py=p(2);pz=p(3);
-        [angles,flags]=computeInverseKinematics(px, py, pz, LegNo, robot);
-        angles=mapAngleAroundZero(angles);
-        minAngles=mapAngleAroundZero(minAngles);
-        maxAngles=mapAngleAroundZero(maxAngles);
-        
-        sol1Check=(minAngles(1) <= angles(1,1) && angles(1,1) <= maxAngles(1)) &&...
-        (minAngles(2) <= angles(1,2) && angles(1,2) <= maxAngles(2)) &&...
-        (minAngles(3) <= angles(1,3) && angles(1,3) <= maxAngles(3));
-    
-        sol2Check=(minAngles(1) <= angles(2,1) && angles(2,1) <= maxAngles(1)) &&...
-            (minAngles(2) <= angles(2,2) && angles(2,2) <= maxAngles(2)) &&...
-            (minAngles(3) <= angles(2,3) && angles(2,3) <= maxAngles(3));
-        
-        if (sol1Check && ~logical(flags(1))) || (sol2Check && ~logical(flags(2)))
-            feasible=1;
-        else
-            feasible=0;
-        end
-        
-    end
+angles=[mapAngle1AroundZero(angles(:,1)) mapAngleAroundZero(angles(:,2:3))];
+minAngles=[mapAngle1AroundZero(minAngles(1)) mapAngleAroundZero(minAngles(2:3))];
+maxAngles=[mapAngle1AroundZero(maxAngles(1)) mapAngleAroundZero(maxAngles(2:3))];
+
+sol1Check=(minAngles(1) <= angles(1,1) && angles(1,1) <= maxAngles(1)) &&...
+    (minAngles(2) <= angles(1,2) && angles(1,2) <= maxAngles(2)) &&...
+    (minAngles(3) <= angles(1,3) && angles(1,3) <= maxAngles(3));
+
+sol2Check=(minAngles(1) <= angles(2,1) && angles(2,1) <= maxAngles(1)) &&...
+    (minAngles(2) <= angles(2,2) && angles(2,2) <= maxAngles(2)) &&...
+    (minAngles(3) <= angles(2,3) && angles(2,3) <= maxAngles(3));
+
+if (sol1Check && ~logical(flags(1))) || (sol2Check && ~logical(flags(2)))
+    feasible=1;
+else
+    feasible=0;
+end
+
 end
 
 function thMapped=mapAngleAroundZero(th)
@@ -55,3 +50,10 @@ thMapped=th*pi;
 
 end
 
+function thMapped=mapAngle1AroundZero(th)
+
+th=th/pi;
+th=mod(th+.5,2)-.5;
+thMapped=th*pi;
+
+end
