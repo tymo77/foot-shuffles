7d7df14604099dcd02af02767691b679b38bd8da
Tyler Morrison <tylermorrison7777@gmail.com>
2018-01-07 21:33:25 -0500

    added handling for exact duplicates in paretoMin
    Exact duplicates should be returned together as part of the pareto front if they
    are pareto-optimal. Changes to paretoMin allow for this.

--- optimization/paretoMin.m
+++ optimization/paretoMin.m
@@ -4,16 +4,36 @@ if size(listPairs,2)~=2
     error('paretoMin is meant to compare a list of pairs')
 end
 
+%remove NaN entries
+listPairs(any(isnan(listPairs),2),:)=[];
+
 N=size(listPairs,1);
 test=false(N,1);
 
 
+
+
 for i=1:N
     indices=1:N;
-    indices(i)=[];
-
+    indices(si)=[];
+    
+    %pareto condition
     test(i)=~any(all(listPairs(indices,:)<=listPairs(i,:),2));
-
+    
+    %check for exact duplicate
+    if any(all(listPairs(indices,:)==listPairs(i,:),2))
+        
+        %if the duplicate pairs are pareto-optimal when compared to their
+        %non-duplicates, then include them in the pareto-front
+        duplicateIndices=indices(~all(listPairs(indices,:)==listPairs(i,:),2));
+        
+        dupInPareto=~any(all(listPairs(duplicateIndices,:)<=listPairs(i,:),2));
+        if dupInPareto
+            test(i)=true;
+            test(duplicateIndices)=true;
+        end
+    end
+    
 end
 
 pareto=listPairs(test,:);
