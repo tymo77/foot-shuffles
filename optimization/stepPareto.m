3b6e81fb02f7e1501b7ac568f5708d49b313775d
Tyler Morrison <tylermorrison7777@gmail.com>
2018-01-07 21:34:16 -0500

    add function for calculating the pareto-front from the set of optimized shuffle results

---
+++ optimization/stepPareto.m
@@ -0,0 +1,30 @@
+function [paretoFront, indices]=stepPareto(xstars,orders,init,fResult,nResult,regions,robot)
+%initialize foot positions
+
+N=length(orders);
+endStab=zeros(N,1)+NaN;
+noSteps=zeros(N,1);
+for i=1:N
+    x=init;
+    order=orders{i};
+    noSteps(i)=length(order);
+    
+    %skip those values which returned an empty optimum
+    if numel(xstars{i})>0
+        %convert update foot positions with xyz point
+        for j=1:noSteps(i)
+            
+            
+            
+            x(order(j),:)=postProcess(xstars{i}(j,:),regions,order(j));
+            
+        end
+        
+        %compute end-stability
+        endStab(i)=angularStabilityMargin(robot.bodyPos, x, fResult, nResult);
+    end
+end
+
+[paretoFront,indices]=paretoMinMax([noSteps endStab])
+
+end
